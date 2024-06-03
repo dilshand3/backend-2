bd527985262742274451eb7def9640972d6f10bf
dilshan.d3 <dilshan72404@gmail.com>
2024-06-03 12:45:01 +0530

    confirm changes

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-const connectdb = async () => {
+const connectDB = async () => {
     try {
         const connetionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOST : ${connetionInstance.connection.host}`)
@@ -11,4 +11,4 @@ const connectdb = async () => {
     }
 }
 
-export default connectdb
+export default connectDB
